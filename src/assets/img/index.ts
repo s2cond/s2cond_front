c784402880b264dd05bd1b1f016b9825a7d44049
illlama <beatlemaniakih@gmail.com>
2021-02-27 11:06:59 +0900

    interest image src add

---
+++ src/assets/img/index.ts
@@ -0,0 +1,165 @@
+import ai from 'assets/img/ai.png';
+import animation from 'assets/img/animation.png';
+import art from 'assets/img/art.png';
+import babyBoomer from 'assets/img/babyBoomer.png';
+import backhand from 'assets/img/backhand.png';
+import baseball from 'assets/img/baseball.png';
+import basketball from 'assets/img/basketball.png';
+import biology from 'assets/img/biology.png';
+import blockchain from 'assets/img/blockchain.png';
+import chillVibes from 'assets/img/chillVibes.png';
+import chinese from 'assets/img/chinese.png';
+import climate from 'assets/img/climate.png';
+import craft from 'assets/img/craft.png';
+import crystallball from 'assets/img/crystallball.png';
+import cycling from 'assets/img/cycling.png';
+import danceFun from 'assets/img/danceFun.png';
+import dating from 'assets/img/dating.png';
+import design from 'assets/img/design.png';
+import disabled from 'assets/img/disabled.png';
+import eCommerce from 'assets/img/eCommerce.png';
+import economics from 'assets/img/economics.png';
+import education from 'assets/img/blockchain.png';
+import english from 'assets/img/blockchain.png';
+import entertainment from 'assets/img/blockchain.png';
+import entrepreneurship from 'assets/img/entrepreneurship.png';
+import fashion from 'assets/img/fashion.png';
+import flim from 'assets/img/flim.png';
+import finance from 'assets/img/finance.png';
+import fire from 'assets/img/fire.png';
+import fitness from 'assets/img/fitness.png';
+import food from 'assets/img/food.png';
+import foodDrink from 'assets/img/foodDrink.png';
+import french from 'assets/img/french.png';
+import game from 'assets/img/game.png';
+import genX from 'assets/img/genX.png';
+import genZ from 'assets/img/genZ.png';
+import german from 'assets/img/german.png';
+import golf from 'assets/img/golf.png';
+import healthcare from 'assets/img/healthcare.png';
+import healthNutrition from 'assets/img/healthNutrition.png';
+import history from 'assets/img/healthNutrition.png';
+import hr from 'assets/img/healthNutrition.png';
+import indonesian from 'assets/img/healthNutrition.png';
+import japanese from 'assets/img/healthNutrition.png';
+import lgbtq from 'assets/img/healthNutrition.png';
+import marketing from 'assets/img/healthNutrition.png';
+import math from 'assets/img/healthNutrition.png';
+import millennials from 'assets/img/healthNutrition.png';
+import mobility from 'assets/img/healthNutrition.png';
+import music from 'assets/img/healthNutrition.png';
+import networking from 'assets/img/healthNutrition.png';
+import outdoor from 'assets/img/healthNutrition.png';
+import parenting from 'assets/img/healthNutrition.png';
+import pet from 'assets/img/healthNutrition.png';
+import philosophy from 'assets/img/healthNutrition.png';
+import photography from 'assets/img/healthNutrition.png';
+import physics from 'assets/img/healthNutrition.png';
+import psycholgy from 'assets/img/healthNutrition.png';
+import readingMovie from 'assets/img/healthNutrition.png';
+import realEstate from 'assets/img/healthNutrition.png';
+import robotics from 'assets/img/healthNutrition.png';
+import running from 'assets/img/healthNutrition.png';
+import saas from 'assets/img/healthNutrition.png';
+import shopping from 'assets/img/healthNutrition.png';
+import soccer from 'assets/img/healthNutrition.png';
+import space from 'assets/img/healthNutrition.png';
+import spanish from 'assets/img/healthNutrition.png';
+import startup from 'assets/img/healthNutrition.png';
+import stock from 'assets/img/healthNutrition.png';
+import streaming from 'assets/img/healthNutrition.png';
+import swim from 'assets/img/healthNutrition.png';
+import tennis from 'assets/img/healthNutrition.png';
+import theater from 'assets/img/healthNutrition.png';
+import theFuture from 'assets/img/healthNutrition.png';
+import traveling from 'assets/img/healthNutrition.png';
+import veganism from 'assets/img/healthNutrition.png';
+import ventureAngel from 'assets/img/healthNutrition.png';
+import victoryHand from 'assets/img/healthNutrition.png';
+import vrAr from 'assets/img/healthNutrition.png';
+import wedding from 'assets/img/healthNutrition.png';
+import writing from 'assets/img/healthNutrition.png';
+
+export const interestImg = {
+  ai,
+  animation,
+  art,
+  babyBoomer,
+  backhand,
+  baseball,
+  basketball,
+  biology,
+  blockchain,
+  chillVibes,
+  chinese,
+  climate,
+  craft,
+  crystallball,
+  cycling,
+  danceFun,
+  dating,
+  design,
+  disabled,
+  eCommerce,
+  economics,
+  education,
+  english,
+  entertainment,
+  entrepreneurship,
+  fashion,
+  flim,
+  finance,
+  fire,
+  fitness,
+  food,
+  foodDrink,
+  french,
+  game,
+  genX,
+  genZ,
+  german,
+  golf,
+  healthcare,
+  healthNutrition,
+  history,
+  hr,
+  indonesian,
+  japanese,
+  lgbtq,
+  marketing,
+  math,
+  millennials,
+  mobility,
+  music,
+  networking,
+  outdoor,
+  parenting,
+  pet,
+  philosophy,
+  photography,
+  physics,
+  psycholgy,
+  readingMovie,
+  realEstate,
+  robotics,
+  running,
+  saas,
+  shopping,
+  soccer,
+  space,
+  spanish,
+  startup,
+  stock,
+  streaming,
+  swim,
+  tennis,
+  theater,
+  theFuture,
+  traveling,
+  veganism,
+  ventureAngel,
+  victoryHand,
+  vrAr,
+  wedding,
+  writing,
+};
